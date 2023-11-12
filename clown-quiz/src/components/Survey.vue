4f87877c2089bfce99bdc6d75b2079ff52eacdca
BananaGrill <bea_19962@hotmail.com>
2023-11-12 17:39:11 +0000

    moving questions

--- clown-quiz/src/components/Survey.vue
+++ clown-quiz/src/components/Survey.vue
@@ -1,32 +1,35 @@
 <template>
-  <div v-for="question in questions" :key="question.id" class="survey">
-    {{ question.id }} - {{ question.question }}
-    <div v-for="answer in question.answers" :key="answer.id" class="answers">
-      {{ answer.text }}
+  <div class="survey" v-if="surveyStore.currentQuestion">
+    <div :key="surveyStore.currentQuestion.id">
+      {{ surveyStore.currentQuestion.id }} - {{ surveyStore.currentQuestion.question }}
+      <div v-for="answer in surveyStore.currentQuestion.answers" :key="answer.text" class="answers">
+        {{ answer.text }}
+      </div>
     </div>
+    <button @click="surveyStore.prevQuestion" :disabled="isFirstQuestion">
+      Previous
+    </button>
+    <button @click="surveyStore.nextQuestion" :disabled="isLastQuestion">
+      Next
+    </button>
   </div>
 </template>
 
-<script>
-import { ref, onMounted } from 'vue'
+<script setup>
+import { onMounted } from 'vue';
+import { useSurveyStore } from '../stores/surveyStore';
 
-export default {
-  setup() {
-    const questions = ref([])
+const surveyStore = useSurveyStore();
 
-    onMounted(async () => {
-      import('../questions.json')
-        .then((module) => {
-          questions.value = module.default
-        })
-        .catch((error) => {
-          console.error('Error importing questions.json:', error)
-        })
-    })
-
-    return {
-      questions
-    }
+onMounted(async () => {
+  if (surveyStore.questions.length === 0) { // Only load questions if they haven't been loaded yet
+    import('../questions.json')
+      .then((module) => {
+        surveyStore.loadQuestions(module.default);
+      })
+      .catch((error) => {
+        console.error('Error importing questions.json:', error);
+      });
   }
-}
+});
 </script>
--- clown-quiz/src/stores/surveyStore.js
+++ clown-quiz/src/stores/surveyStore.js
@@ -2,45 +2,30 @@ import { defineStore } from 'pinia'
 
 export const useSurveyStore = defineStore('survey', {
   state: () => ({
-    scores: {
-      Classical: 0,
-      Mime: 0,
-      Jester: 0,
-      SillyBilly: 0,
-      Rodeo: 0
-    },
-    currentQuestionIndex: 0
-  }),
-  getters: {
-    // Define any getters to compute derived state here
+    questions: [],
+    currentQuestionIndex:0,
+}),
+getters: {
+  currentQuestion: (state) => state.questions[state.currentQuestionIndex] || null,
+},
+actions: {
+  loadQuestions(questionData){
+    this.questions = questionData;
   },
-  actions: {
-    resetScores() {
-      this.scores = { Classical: 0, Mime: 0, Jester: 0, SillyBilly: 0, Rodeo: 0 }
-    },
-    // Increment the score for a clown type
-    incrementScore(clownType, value) {
-      if (this.scores.hasOwnProperty(clownType)) {
-        this.scores[clownType] += value
-      }
-    },
-    nextQuestion() {
-      this.currentQuestionIndex++
-    },
-    // get the highest scoring clown type
-    determineClownType() {
-      const clownTypes = Object.keys(this.scores)
-      let maxScore = -Infinity
-      let maxType = ''
-
-      clownTypes.forEach((type) => {
-        if (this.scores[type] > maxScore) {
-          maxScore = this.scores[type]
-          maxType = type
-        }
-      })
-
-      return maxType
+  nextQuestion(){
+    if(this.currentQuestionIndex < this.questions.length-1){
+      this.currentQuestionIndex++;
+    }
+  },
+  prevQuestion(){
+    if(this.currentQuestionIndex > 0){
+      this.currentQuestionIndex--;
+    }
+  },
+  setQuestionIndex(index) {
+    if(index >= 0 && index < this.questions.length){
+      this.currentQuestionIndex = index
     }
   }
+}
 })
