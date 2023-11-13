b3cafebd73a216844e7a7980b0a96daaa4a50f80
BananaGrill <bea_19962@hotmail.com>
2023-11-13 20:03:43 +0000

    score fix

--- clown-quiz/src/views/ResultView.vue
+++ clown-quiz/src/views/ResultView.vue
@@ -1,5 +1,20 @@
 <template>
  <div class="results">
   YOU ARE A CLOWN
+  <button @click=retryQuiz()>Retry</button>
  </div>
 </template>
+
+<script setup>
+import { useSurveyStore } from '../stores/surveyStore'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const surveyStore = useSurveyStore()
+
+function retryQuiz() {
+ surveyStore.resetScores()
+  router.push('/')
+}
+</script>
